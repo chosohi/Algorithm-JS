e515d6e5f1b531653190f45d37e4060d8bbc72f8
huisso97 <chosohi@naver.com>
2022-12-30 13:52:26 +0900

    폴더 정리

--- Udemy/Section 18 - 자료 구조 소개/Class keyword.js
+++ Udemy/Section 18 - 자료 구조 소개/Class keyword.js
@@ -26,20 +26,3 @@ class Student {
 let firstStudent = new Student("Colt", "Steele");
 let secondStudent = new Student("Blue", "Steele");
 firstStudent.marklate();
-
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  static distance(point1, point2) {
-    const dx = point1.x - point2.x;
-    const dy = point1.y - point2.y;
-    return Math.hypot(dx, dy);
-  }
-}
-
-const p1 = new Point(5, 5);
-const p2 = new Point(10, 10);
-Point.distance(p1, p2);
-// 7.0710678118654755;
