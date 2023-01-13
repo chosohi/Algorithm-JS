a0d01a1aa95f84d573faec620b631a66bb88530d
huisso97 <chosohi@naver.com>
2023-01-13 12:22:02 +0900

    Section22: BST insert

---
+++ Udemy/Section 22 - 이진 검색 트리/BinarySearchTree Class.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        // 동일한 값이 있다면
+        if (value === current.value) return undefined;
+        // 루트보다 값이 작다면
+        if (value < current.value) {
+          // 루트의 왼쪽 노드가 없다면
+          if (current.left === null) {
+            current.left = newNode;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
