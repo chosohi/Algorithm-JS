b2ad01ae82523e52c01fd15f181e4ee0d0548516
chosohi <chosohi@naver.com>
2023-03-14 23:03:42 +0900

    inflearn: 공통원소 구하기

--- Inflearn/5. 효율성(투포인터 알고리즘, 슬라이딩 윈도우, 해쉬)/공통원소 구하기.js
+++ Inflearn/5. 효율성(투포인터 알고리즘, 슬라이딩 윈도우, 해쉬)/공통원소 구하기.js
@@ -1,6 +1,20 @@
 // 교집합을 오름차순으로 정렬하는 문제
 
 function solution(arr1, arr2) {
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  let p1 = 0,
+    p2 = 0;
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+
   return answer;
 }
 
