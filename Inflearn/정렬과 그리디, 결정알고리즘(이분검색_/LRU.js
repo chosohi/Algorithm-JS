313c77249029ee2b848e7c1db51517f86b18cb32
chosohi <chosohi@naver.com>
2023-04-01 20:00:27 +0900

    inflearn: LRU

--- Inflearn/정렬과 그리디, 결정알고리즘(이분검색_/LRU.js
+++ Inflearn/정렬과 그리디, 결정알고리즘(이분검색_/LRU.js
@@ -1,3 +1,29 @@
+function setQueue(queue, val) {
+  if (queue.includes(val)) {
+    queue = queue
+      .slice(0, queue.indexOf(val))
+      .concat(queue.slice(queue.indexOf(val) + 1));
+    queue.unshift(val);
+  } else {
+    queue.pop();
+    queue.unshift(val);
+  }
+  return queue;
+}
+
+function solution(s, n, arr) {
+  let queue = new Array(s).fill(0);
+
+  for (const val of arr) {
+    if (queue.length === s) {
+      queue = setQueue(queue, val);
+    } else {
+      queue = setQueue(queue, val);
+    }
+  }
+  return queue.join(" ");
+}
+
 console.log(solution(5, 9, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
 // 10000
 // 21000
@@ -8,4 +34,3 @@ console.log(solution(5, 9, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
 // 32610
 // 53261
 // 75326
-// test
